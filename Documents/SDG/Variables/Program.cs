e7ce68faf773657110a8a8219f9b7a721bf45f9c
poe1993 <poe1993@yahoo.com>
2020-08-11 11:40:47 -0400

    Variable Examples

---
+++ Documents/SDG/Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OurDotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to My Program!");
+
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n");
+
+            Console.Write("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"It is a pleasure to meet you, {name}");
+
+            var score = 98;
+
+            Console.WriteLine($"Your score is {2 * score - 5}");
+
+
+
+        }
+    }
+}
